4d1fc1447d375a1ccdc58698df05dafdc4f384fb
TomBen <mr.ren.tao123@gmail.com>
2024-06-06 10:29:20 +0800

    Update citation-backlinks

--- _extensions/citation-backlinks/citation-backlinks.lua
+++ _extensions/citation-backlinks/citation-backlinks.lua
@@ -1,8 +1,6 @@
--- Todo: even with link-citations and link-fields turned off, citation-backlinks still adds a single backlink to each reference in the final bibliography div.
-
 --- citation-backlinks.lua – adds citation backlinks to the bibliography
 ---
---- Copyright: © 2023 John MacFarlane and Albert Krewinkel and Bernardo Vasconcelos
+--- Copyright: © 2022 John MacFarlane and Albert Krewinkel
 --- License: MIT – see LICENSE for details
 
 -- Makes sure users know if their pandoc version is too old for this
@@ -16,15 +14,10 @@ local cites = {}
 -- counter for cite identifiers
 local cite_number = 1
 
-local function with_label(s, el)
+local function with_latex_label(s, el)
   if FORMAT == "latex" then
-    -- return {pandoc.RawInline("latex", "\\pagelabel{" .. s .. "}"), el}
     return {pandoc.RawInline("latex", "\\label{" .. s .. "}"), el}
-  end
-  if FORMAT == "typst" then
-    return {el, pandoc.RawInline("typst", " #label(\"" .. s .. "\")")}
-  end
-  if FORMAT == "docx" or FORMAT == "html" then
+  else
     return {el}
   end
 end
@@ -39,11 +32,7 @@ function Cite(el)
       cites[citation.id] = {cite_id}
     end
   end
-  if FORMAT == "typst" then
-    return pandoc.Span(with_label(cite_id, el))
-  else
-    return pandoc.Span(with_label(cite_id, el), pandoc.Attr(cite_id))
-  end
+  return pandoc.Span(with_latex_label(cite_id, el), pandoc.Attr(cite_id))
 end
 
 function append_inline(blocks, inlines)
@@ -60,15 +49,8 @@ end
 
 function Div(el)
   local citation_id = el.identifier:match("ref%-(.+)")
-  local tex_return_link = pandoc.RawInline("latex", "\\Acrobatmenu{GoBack}{$\\hookleftarrow$}")
-
   if citation_id then
-    local backlinks = pandoc.Inlines{pandoc.Space(), pandoc.Str("[")}
-    if FORMAT == "latex" then
-      -- quarto.doc.include_text("in-header", [[ \newcommand\pagelabel{\phantomsection\label} ]])
-      table.insert(backlinks, tex_return_link)
-    end
-
+    local backlinks = pandoc.Inlines{pandoc.Str("Cited:"),pandoc.Space()}
     for i,cite_id in ipairs(cites[citation_id] or {}) do
       local marker = pandoc.Str(i)
       if FORMAT == "latex" then
@@ -81,7 +63,7 @@ function Div(el)
       table.insert(backlinks, pandoc.Link(marker, "#"..cite_id))
     end
     if #backlinks > 2 then
-      append_inline(el.content, backlinks .. {pandoc.Str("]")})
+      append_inline(el.content, {pandoc.Space()} .. backlinks)
     end
     return el
   end
